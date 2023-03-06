9ecb0594aa57c6cfec209cb6211ad8d013e7f805
MasterChief <tesluknikita@gmail.com>
2023-03-06 22:57:14 +0300

    Added sample of JTable

---
+++ src/Table.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import javax.swing.JTable;
+
+public class Table {
+    private JPanel panel1;
+    private JTable table1;
+    private JButton button1;
+    private JTextField textField1;
+}
